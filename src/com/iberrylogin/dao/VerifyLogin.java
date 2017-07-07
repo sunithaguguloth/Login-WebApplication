4632b28c6b7ca5e7d03ad833e7e08374d2c49a77
Unitech17 <Unitech17@Unitech18-PC>
2017-07-07 16:17:16 +0530

    VerifyLogin-G.Sunitha

--- src/com/iberrylogin/dao/VerifyLogin.java
+++ src/com/iberrylogin/dao/VerifyLogin.java
@@ -1,17 +1,66 @@
-package com.iberrylogin.dao;
+package com.iberrylogin.servlets;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class VerifyLogin {
-    public static boolean validate(String name, String pass) {
-        //This program validates the user id and password
-    	boolean status = false;
-        
-        return status;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.iberrylogin.dao.VerifyLogin;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
     }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		// TODO Auto-generated method stub
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out=response.getWriter();
+		response.setContentType("text/html");
+		String un=request.getParameter("Username");
+		String ps=request.getParameter("Password");
+	boolean x= VerifyLogin.validate(un ,ps);
+	if(x)
+	{
+		RequestDispatcher rd=request.getRequestDispatcher("welcome.jsp");
+		rd.forward(request,response);
+	}
+	else
+	{
+		RequestDispatcher rd=request.getRequestDispatcher("error.jsp");
+		rd.forward(request,response);
+	
+	}
+	}
+		
+	//response.getWriter().append("Served at: ").append(request.getContextPath());
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
 }
